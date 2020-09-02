b94d548b8c7e1370efcb330ebf1f6509a15635f0
juein <apocriz@gmail.com>
2020-09-02 12:48:55 +0900

    swiper 적용 테스트

--- es6/main.js
+++ es6/main.js
@@ -10,7 +10,13 @@ window.onload = function(){
 // onload
 
 
+
 window.addEventListener('DOMContentLoaded', function(){ 
+
+    let pageNum = 0;
+    let isAnimating = false;
+    let verticallSwiper;
+    
 
     //리로드시 최상단으로
     window.onbeforeunload = function () {
@@ -20,28 +26,136 @@ window.addEventListener('DOMContentLoaded', function(){
 
     const ctrl = new ScrollMagic.Controller();
 
+    //swiper 제어
+    //verticallSwiper.slideTo(0, 500);
+    //verticallSwiper.slideNext();
+    //goToSlide(0);
 
-    const target = { a: 1, b: 2 };
-    const source = { b: 4, c: 5 };
-
-    const returnedTarget = Object.assign(target, source);
-
-    console.log(target);
-    // expected output: Object { a: 1, b: 4, c: 5 }
-
-    console.log(returnedTarget);
-    // expected output: Object { a: 1, b: 4, c: 5 } 
-
-    // ES6
-    const arr = [1, 2, 3];
-    const pow = arr.map(x => x * x);
-
-    console.log(pow); // [ 1, 4, 9 ]
-
-
-    //document.querySelector('.test').innerHTML = 'test123';
-
+    //content swiper
+    verticallSwiper = new Swiper('.content__container', {
+        on: {
+          slideChangeTransitionStart: function(){
+            var idx = this.realIndex;
+            console.log('vertical idx : ' + idx);
+          },
+        },
+        direction: 'vertical',
+        autoplay: false,
+        //keyboard : true,
+        //allowTouchMove : false,
+        //mousewheelControl: true,
+        //mousewheel: {
+        //  invert: false,
+        //},
+        speed: 800,
+    });
 
 
-})
+    //page controll
+    function goToNextSlide(){
+        if( pageNum >= 15) return false; //총 페이지 수를 넘어가면 false처리
+
+        if(!isAnimating){
+            isAnimating = true;
+            pageNum += 1;
+
+            console.log('page = ' + pageNum);
+
+            
+            verticallSwiper.slideTo(pageNum, 500);
+            //verticallSwiper.slideNext();
+
+            setTimeout(() => {
+                onSlideChangeEnd();
+            }, 1000)
+            
+
+            
+            //switch
+            /*
+            switch(pageNum){
+                case 0:
+                    break;
+                case 1:
+                    console.log('page = ' + pageNum);
+                    setTimeout(() => {
+                        onSlideChangeEnd();
+                    }, 1000);
+                    break;
+                case 2:
+                    console.log('page = ' + pageNum);
+                    setTimeout(() => {
+                        onSlideChangeEnd();
+                    }, 1000);
+                    break;
+                default:
+                    //onSlideChangeEnd();
+                    break;
+            }
+            */
+            
+        }
+    };
+
+    
+    function goToPrevSlide(){
+        if(pageNum <= 0) return false; //index: 0 기준 false
+
+        if(!isAnimating){
+            isAnimating = true;
+            pageNum -= 1;
+
+            console.log('page = ' + pageNum);
+
+            //verticallSwiper.slidePrev();
+            verticallSwiper.slideTo(pageNum, 500);
+
+            setTimeout(() => {
+                onSlideChangeEnd();
+            }, 1000)
+
+        }
+    };
+    
+
+    function onSlideChangeEnd(){
+        console.log('isAnimating == false');
+        isAnimating = false;
+    }
+
+    //Math.sign 함수 대체
+    //function sgn(x) {
+    //    return (x > 0) - (x < 0);
+    //}
+
+    // scroll controll
+    window.addEventListener("wheel", function (event) {
+        //let delta = sgn(event.deltaY);
+        let delta = Math.sign(event.deltaY);
+        
+        if(delta > 0){ //down
+        goToNextSlide();
+        }else { //up
+            goToPrevSlide();
+        }
+    });
+
+
+    // key controll
+    const keyCodes = {
+        UP  : 38,
+        DOWN: 40
+    }
+    window.addEventListener("keydown", function (event) {
+        let PRESSED_KEY = event.keyCode;
+        if(PRESSED_KEY == keyCodes.DOWN){
+            goToNextSlide();
+            event.preventDefault();
+        }else if(PRESSED_KEY == keyCodes.UP){
+            goToPrevSlide();
+            event.preventDefault();
+        }
+    });
+
+});
 
--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ window.onload = function () {}
 
 ;window.addEventListener('DOMContentLoaded', function () {
 
+    var pageNum = 0;
+    var isAnimating = false;
+    var verticallSwiper = void 0;
+
     //리로드시 최상단으로
     window.onbeforeunload = function () {
         //window.scrollTo(0, 0);
@@ -22,26 +26,127 @@ window.onload = function () {}
 
     var ctrl = new ScrollMagic.Controller();
 
-    var target = { a: 1, b: 2 };
-    var source = { b: 4, c: 5 };
+    //swiper 제어
+    //verticallSwiper.slideTo(0, 500);
+    //verticallSwiper.slideNext();
+    //goToSlide(0);
 
-    var returnedTarget = Object.assign(target, source);
-
-    console.log(target);
-    // expected output: Object { a: 1, b: 4, c: 5 }
-
-    console.log(returnedTarget);
-    // expected output: Object { a: 1, b: 4, c: 5 } 
-
-    // ES6
-    var arr = [1, 2, 3];
-    var pow = arr.map(function (x) {
-        return x * x;
+    //content swiper
+    verticallSwiper = new Swiper('.content__container', {
+        on: {
+            slideChangeTransitionStart: function slideChangeTransitionStart() {
+                var idx = this.realIndex;
+                console.log('vertical idx : ' + idx);
+            }
+        },
+        direction: 'vertical',
+        autoplay: false,
+        //keyboard : true,
+        //allowTouchMove : false,
+        //mousewheelControl: true,
+        //mousewheel: {
+        //  invert: false,
+        //},
+        speed: 800
     });
 
-    console.log(pow); // [ 1, 4, 9 ]
+    //page controll
+    function goToNextSlide() {
+        if (pageNum >= 15) return false; //총 페이지 수를 넘어가면 false처리
 
+        if (!isAnimating) {
+            isAnimating = true;
+            pageNum += 1;
 
-    //document.querySelector('.test').innerHTML = 'test123';
+            console.log('page = ' + pageNum);
 
+            verticallSwiper.slideTo(pageNum, 500);
+            //verticallSwiper.slideNext();
+
+            setTimeout(function () {
+                onSlideChangeEnd();
+            }, 1000);
+
+            //switch
+            /*
+            switch(pageNum){
+                case 0:
+                    break;
+                case 1:
+                    console.log('page = ' + pageNum);
+                    setTimeout(() => {
+                        onSlideChangeEnd();
+                    }, 1000);
+                    break;
+                case 2:
+                    console.log('page = ' + pageNum);
+                    setTimeout(() => {
+                        onSlideChangeEnd();
+                    }, 1000);
+                    break;
+                default:
+                    //onSlideChangeEnd();
+                    break;
+            }
+            */
+        }
+    };
+
+    function goToPrevSlide() {
+        if (pageNum <= 0) return false; //index: 0 기준 false
+
+        if (!isAnimating) {
+            isAnimating = true;
+            pageNum -= 1;
+
+            console.log('page = ' + pageNum);
+
+            //verticallSwiper.slidePrev();
+            verticallSwiper.slideTo(pageNum, 500);
+
+            setTimeout(function () {
+                onSlideChangeEnd();
+            }, 1000);
+        }
+    };
+
+    function onSlideChangeEnd() {
+        console.log('isAnimating == false');
+        isAnimating = false;
+    }
+
+    //Math.sign 함수 대체
+    //function sgn(x) {
+    //    return (x > 0) - (x < 0);
+    //}
+
+    // scroll controll
+    window.addEventListener("wheel", function (event) {
+        //let delta = sgn(event.deltaY);
+        var delta = Math.sign(event.deltaY);
+
+        if (delta > 0) {
+            //down
+            goToNextSlide();
+        } else {
+            //up
+            goToPrevSlide();
+        }
+    });
+
+    // key controll
+    var keyCodes = {
+        UP: 38,
+        DOWN: 40
+    };
+    window.addEventListener("keydown", function (event) {
+        var PRESSED_KEY = event.keyCode;
+        if (PRESSED_KEY == keyCodes.DOWN) {
+            goToNextSlide();
+            event.preventDefault();
+        } else if (PRESSED_KEY == keyCodes.UP) {
+            goToPrevSlide();
+            event.preventDefault();
+        }
+    });
 });
